c438864e2afd229c69fe164f2834df1c0ee0be7e
slimkaki <rafaalmada98@gmail.com>
2020-11-20 12:01:02 -0300

    bbb

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        field = ['title', 'pub_date', 'description']
+        field = '__all__' #['title', 'pub_date', 'description']
