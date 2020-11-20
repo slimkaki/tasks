5a45eba655bbba6dbbc474963eb929e1677e44d2
slimkaki <rafaalmada98@gmail.com>
2020-11-20 14:08:14 -0300

    ajustando endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_all', views.get_tasks, name='get_tasks'),
+    path('get_all', views.get_all_tasks, name='get_all_tasks'),
+    path('get_single/<int:id_task>', views.get_single_task , name='get_single_task'),
     path('post', views.post_task, name='post_task'),
+    path('delete/<int:id_task>', views.delete_task , name='delete_task'),
+    path('update/<int:id_task>', views.update_task , name='update_task'),
 ]
