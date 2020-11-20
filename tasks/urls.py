5d96d8a03dbc012338c37bd0318030303c679f59
slimkaki <rafaalmada98@gmail.com>
2020-11-20 10:02:22 -0300

    aprimorando client

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_task/<>/', views.create_task,name='create_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from .models import Task
 
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the tasks index.")
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+def task_list(request):
+    context = {'task_list': Task.objects.all()}
+    return render(request, 'task/task_list.html', context)
+
+def create_task(request: HttpRequest):
+    task=Task(content=request.POST[''])
+    task.save()
+    return redirect('task/list/')
+
+def delete_task(request, task_id):
+    task = Todo.objects.get(id=todo_id)
+    task.delete()
+    return redirect('task/list/')
