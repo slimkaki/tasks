f0da4e6e154ab4c7f07d44c996216067a1bb42e5
slimkaki <rafaalmada98@gmail.com>
2020-11-20 11:48:31 -0300

    a

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_all', views.getTasks, name='getTasks')
-    path('post', views.postTask, name='postTask')
+    path('get_all', views.getTasks, name='getTasks'),
+    path('post', views.postTask, name='postTask'),
 ]
